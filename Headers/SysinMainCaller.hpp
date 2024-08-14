6a20ff7f7bfc8679a09c2b3efe98cd31a6e41fcb
altoliaw <altoliaw@hotmail.com>
2024-08-14 17:28:23 +0800

    Some updating in the vendor folder

--- Headers/SysinMainCaller.hpp
+++ Headers/SysinMainCaller.hpp
@@ -5,22 +5,34 @@
  * @author Nick, Liao
  * @date 2024/05/15
  */
+
+#ifdef __linux__  // Linux
+
 #include <netinet/if_ether.h>  // For Ethernet header structure
 #include <netinet/ip.h>        // For IP header structure
 #include <netinet/tcp.h>       // For TCP header structure
 #include <netinet/udp.h>       // For UDP header structure
-#include <signal.h>            // For the alarm and interrupted signal
-#include <stdio.h>             // For some C io functions
-#include <stdlib.h>            // For some C functions
 #include <unistd.h>            // For sleep and pid functions
+
+#elif defined(_WIN32) || defined(_WIN64)
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#endif
+
+#include <signal.h>  // For the alarm and interrupted signal
+#include <stdio.h>   // For some C io functions
+#include <stdlib.h>  // For some C functions
 
 #include <algorithm>  // For std::max
 #include <mutex>
+#include <sstream>
 #include <string>
 #include <thread>
 #include <unordered_map>
 #include <vector>
-#include <sstream>
 
 #include "../Models/Commons/Headers/IOSpecification.hpp"
 #include "../Models/Commons/Headers/POSIXErrors.hpp"
