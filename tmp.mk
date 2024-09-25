4a39f50f5e59b1b2a4b68a92397bb15e4e54043d
Nick Liao <nick.liao@cobrasonic.com>
2024-09-25 17:41:44 +0800

    refactor(sizing): A bug fix for the substring

--- tmp.mk
+++
@@ -1,5 +0,0 @@
-Commons.Sources	:=	/home/dbsecure/native-sizing/Models/Commons/Sources
-FileParsers.Sources	:=	/home/dbsecure/native-sizing/Models/FileParsers/Sources
-PCAP.Sources	:=	/home/dbsecure/native-sizing/Models/PCAP/Sources
-Sources.SizingController	:=	/home/dbsecure/native-sizing/Sources/SizingController
-Apps			:=/home/dbsecure/native-sizing/Apps
